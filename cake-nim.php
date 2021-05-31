fd3f114e0f96506f9188377ad05e2716063a8350
ChathurikaThathsarani <80465888+ChathurikaThathsarani@users.noreply.github.com>
2021-05-31 18:03:42 +0530

    Update cake-nim.php

--- cake-nim.php
+++ cake-nim.php
@@ -84,7 +84,7 @@
         <meta charset="utf-8">
         <meta name="viewport" content="width=device-width, initial-scale=1.0">
         <link rel="stylesheet" href="styles/common.css">
-        <link rel="stylesheet" href="styles/photography-say.css">
+        <link rel="stylesheet" href="styles/cake-nim.css">
         <link rel="preconnect" href="https://fonts.gstatic.com">
         <link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:ital,wght@0,100;0,200;0,400;0,600;0,700;1,100;1,400&display=swap">
         <script src="https://kit.fontawesome.com/ab07e88f42.js" crossorigin="anonymous"></script>
